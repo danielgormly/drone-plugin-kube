60bcba499182b44bdb7cec9cfa22d72cab21ae4b
Daniel <daniel@ip-192-168-1-16.ap-southeast-2.compute.internal>
2019-03-28 15:40:08 +1100

    Added glide support & kube client

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kubano
+
+A simple Drone-CI plugin for managing Kubernetes deployments.
+
+#### Usage
+
+
+
+#### Development
+- Kubernetes client not yet supported by dep, so we are using 
+[`brew install glide`](https://github.com/Masterminds/glide).
+- Update dependencies with brew `glide update --strip-vendor`
--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"encoding/base64"
+	"log"
 
-	"github.com/golang/example/stringutil"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+// create the connection to kubernetes based on parameters passed in.
+// the kubernetes/client-go project is really hard to understand.
+func (p Plugin) createKubeClient() (*kubernetes.Clientset, error) {
+
+	ca, err := base64.StdEncoding.DecodeString(p.Config.Ca)
+	config := clientcmdapi.NewConfig()
+	config.Clusters["drone"] = &clientcmdapi.Cluster{
+		Server:                   p.Config.Server,
+		CertificateAuthorityData: ca,
+	}
+	config.AuthInfos["drone"] = &clientcmdapi.AuthInfo{
+		Token: p.Config.Token,
+	}
+
+	config.Contexts["drone"] = &clientcmdapi.Context{
+		Cluster:  "drone",
+		AuthInfo: "drone",
+	}
+	//config.Clusters["drone"].CertificateAuthorityData = ca
+	config.CurrentContext = "drone"
+
+	clientBuilder := clientcmd.NewNonInteractiveClientConfig(*config, "drone", &clientcmd.ConfigOverrides{}, nil)
+	actualCfg, err := clientBuilder.ClientConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return kubernetes.NewForConfig(actualCfg)
+}
+
 func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+
 }

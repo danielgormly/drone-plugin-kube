5ca8575d875119a300869c2af90e0a4ab2eed9c9
jbonzo <8647805+jbonzo@users.noreply.github.com>
2020-02-16 21:55:40 -0500

    pinning to same kube client version as servers

--- go.mod
+++ go.mod
@@ -4,29 +4,13 @@ go 1.13
 
 require (
 	github.com/aymerick/raymond v2.0.3-0.20180322193309-b565731e1464+incompatible
-	github.com/davecgh/go-spew v1.1.1
-	github.com/gogo/protobuf v1.2.2-0.20190818160850-4c00d2f19fb9
-	github.com/golang/protobuf v1.3.1
-	github.com/google/gofuzz v1.0.0
-	github.com/googleapis/gnostic v0.0.0-20170729233727-0c5108395e2d
-	github.com/imdario/mergo v0.3.5
-	github.com/json-iterator/go v1.1.7
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
-	github.com/modern-go/reflect2 v1.0.1
-	github.com/spf13/pflag v1.0.3
-	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5
-	golang.org/x/net v0.0.0-20190812203447-cdfb69ac37fc
-	golang.org/x/oauth2 v0.0.0-20170412232759-a6bd8cefa181
-	golang.org/x/sys v0.0.0-20190606165138-5da285871e9c
-	golang.org/x/text v0.3.2
-	golang.org/x/time v0.0.0-20161028155119-f51c12702a4d
-	google.golang.org/appengine v1.6.2-0.20190801181406-fb139bde60fa
-	gopkg.in/inf.v0 v0.9.0
-	gopkg.in/yaml.v2 v2.2.2
-	k8s.io/api v0.0.0-20190313235455-40a48860b5ab
-	k8s.io/apimachinery v0.0.0-20190313205120-d7deff9243b1
-	k8s.io/client-go v11.0.0+incompatible
-	k8s.io/klog v0.4.0
-	k8s.io/utils v0.0.0-20190221042446-c2654d5206da
-	sigs.k8s.io/yaml v1.1.0
+	github.com/gogo/protobuf v1.2.2-0.20190818160850-4c00d2f19fb9 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	google.golang.org/appengine v1.6.2-0.20190801181406-fb139bde60fa // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+	k8s.io/api v0.0.0-20190819141258-3544db3b9e44
+	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
+	k8s.io/client-go v0.0.0-20190819141724-e14f31a72a77
+	k8s.io/klog v0.4.0 // indirect
 )
---
+++ resources.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+
+	appV1 "k8s.io/api/apps/v1"
+	coreV1 "k8s.io/api/core/v1"
+	kubeErrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// CreateOrUpdateDeployment -- Checks if deployment already exists, updates if it does, creates if it doesn't
+func CreateOrUpdateDeployment(clientset *kubernetes.Clientset, namespace string, deployment *appV1.Deployment) error {
+	deploymentExists, err := deploymentExists(clientset, namespace, deployment.Name)
+	if err != nil {
+		return err
+	}
+	if deploymentExists {
+		log.Printf("📦 Found existing deployment '%s'. Updating.", deployment.Name)
+		_, err = clientset.AppsV1().Deployments(namespace).Update(deployment)
+		return err
+	}
+	log.Printf("📦 Creating new deployment '%s'. Updating.", deployment.Name)
+	_, err = clientset.AppsV1().Deployments(namespace).Create(deployment)
+	return err
+}
+
+// deploymentExists -- Updates given deployment in Kubernetes
+func deploymentExists(clientset *kubernetes.Clientset, namespace string, name string) (bool, error) {
+	_, err := clientset.AppsV1().Deployments(namespace).Get(name, metaV1.GetOptions{})
+	if err != nil {
+		// TODO: Only conver to StatusError if the error is in fact a status error
+		statusError, ok := err.(*kubeErrors.StatusError)
+		if ok && statusError.Status().Code == http.StatusNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+const (
+	stateUpdated = "📦 Updated"
+	stateFailed  = "⛔️ Failed"
+)
+
+// waitUntilDeploymentSettled -- Waits until ready, failure or timeout
+func waitUntilDeploymentSettled(clientset *kubernetes.Clientset, namespace string, name string, timeoutInSeconds int64) (string, error) {
+	fieldSelector := strings.Join([]string{"metadata.name", name}, "=")
+	watchOptions := metaV1.ListOptions{
+		FieldSelector: fieldSelector,
+		Watch:         true,
+	}
+
+	watcher, err := clientset.AppsV1().Deployments(namespace).Watch(watchOptions)
+	if err != nil {
+		return stateFailed, fmt.Errorf("watch deployment; %w", err)
+	}
+
+	liveDeployment, err := clientset.AppsV1().Deployments(namespace).Get(name, metaV1.GetOptions{})
+	if err != nil {
+		return stateFailed, fmt.Errorf("get deployment; %w", err)
+	}
+
+	log.Printf("📦 Unavailable replicas: %d", liveDeployment.Status.UnavailableReplicas)
+	if liveDeployment.Status.UnavailableReplicas == 0 {
+		return "📦 Updated", nil
+	}
+
+	timer := time.NewTimer(time.Duration(timeoutInSeconds) * time.Second)
+	for {
+		select {
+		case <-timer.C:
+			return stateFailed, errors.New("deployment watcher timed out. Something is wrong")
+		case event := <-watcher.ResultChan():
+			deployment := event.Object.(*appV1.Deployment)
+			if deployment.Status.UnavailableReplicas == 0 {
+				return stateUpdated, nil
+			}
+			log.Printf("📦 Unavailable replicas: %d", deployment.Status.UnavailableReplicas)
+		}
+	}
+}
+
+// ApplyService creates a service if it doesn't exists, updates it if it does
+func ApplyService(clientset *kubernetes.Clientset, namespace string, service *coreV1.Service) error {
+	existingService, exists, err := getService(clientset, namespace, service.Name)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		_, err = clientset.CoreV1().Services(namespace).Update(existingService)
+		return err
+	}
+
+	_, err = clientset.CoreV1().Services(namespace).Create(service)
+	return err
+}
+
+// getService returns a service object, or false if it doesn't exist
+func getService(clientset *kubernetes.Clientset, namespace string, name string) (*coreV1.Service, bool, error) {
+	service, err := clientset.CoreV1().Services(namespace).Get(name, metaV1.GetOptions{})
+	if err != nil {
+		statusError, ok := err.(*kubeErrors.StatusError)
+		if ok && statusError.Status().Code == http.StatusNotFound {
+			return nil, false, nil
+		}
+		return nil, false, err
+	}
+
+	return service, true, nil
+}
+
+// ApplyConfigMapFromFile -- Updates given deployment in Kubernetes
+func ApplyConfigMapFromFile(clientset *kubernetes.Clientset, namespace string, configMap *coreV1.ConfigMap, path string) error {
+	log.Printf("📦 Reading contents of %s", path)
+	_, filename := filepath.Split(path)
+	fileContents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	configMapData := make(map[string]string)
+	configMapData[filename] = string(fileContents)
+	configMap.Data = configMapData
+
+	// Check if deployment exists
+	exists, err := configMapExists(clientset, namespace, configMap.Name)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		log.Printf("📦 Found existing deployment. Updating %s.", configMap.Name)
+		_, err = clientset.CoreV1().ConfigMaps(namespace).Update(configMap)
+		return err
+	}
+
+	_, err = clientset.CoreV1().ConfigMaps(namespace).Create(configMap)
+	return err
+}
+
+// configMapExists -- Updates given deployment in Kubernetes
+func configMapExists(clientset *kubernetes.Clientset, namespace string, name string) (bool, error) {
+	_, err := clientset.CoreV1().ConfigMaps(namespace).Get(name, metaV1.GetOptions{})
+	if err != nil {
+		statusError, ok := err.(*kubeErrors.StatusError)
+		if ok && statusError.Status().Code == http.StatusNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}

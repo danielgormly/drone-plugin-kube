cd888d5ff86d1900a233edd70d4e9456ecaf3011
Daniel Gormly <daniel@birdi.com.au>
2020-04-03 22:19:00 +1100

    Updated with service example, updated version number, build steps

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		},
 	}
 	fmt.Printf(os.Getenv("PLUGIN_SKIP_TLS"))
-	fmt.Println("danielgormly/drone-plugin-kube@0.1.0 https://github.com/danielgormly/drone-plugin-kube")
+	fmt.Println("danielgormly/drone-plugin-kube@0.2.0 https://github.com/danielgormly/drone-plugin-kube")
 	err := plugin.Exec()
 	if err != nil {
 		log.Fatalf("⛔️ Fatal error: \n%s", err)
---
+++ test-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PLUGIN_TEMPLATE=test/service.template.yaml
+export PLUGIN_NAME=drone-kube-test
+export PLUGIN_NAMESPACE=default
+
+go build -o build/kubano
+export $(cat .env | xargs) && ./build/kubano
+
+# docker run --env-file=.env drone-kubano
